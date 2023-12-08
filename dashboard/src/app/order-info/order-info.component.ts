06c4a6d1fcb5ab5167ef140786fce65072958111
UsmanDrigrocha <UsmanDrigrocha@Gmail.com>
2023-12-08 19:09:37 +0500

    getting product id from params : order-info

--- dashboard/src/app/order-info/order-info.component.ts
+++ dashboard/src/app/order-info/order-info.component.ts
@@ -1,10 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-order-info',
   templateUrl: './order-info.component.html',
-  styleUrls: ['./order-info.component.css']
+  styleUrls: ['./order-info.component.css'],
 })
 export class OrderInfoComponent {
-
+  constructor(
+    private http: HttpClient,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+  ngOnInit() {
+    this.route.params.subscribe((params) => {
+      const productId = params['id'];
+      console.log(productId)
+    });
+  }
 }
