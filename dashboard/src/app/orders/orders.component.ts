76d6dc0a93776f297a43dbc4e7679e6726c732be
UsmanDrigrocha <UsmanDrigrocha@Gmail.com>
2023-12-08 17:10:15 +0500

    getting Orders : Done

--- dashboard/src/app/orders/orders.component.ts
+++ dashboard/src/app/orders/orders.component.ts
@@ -1,10 +1,38 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
-  styleUrls: ['./orders.component.css']
+  styleUrls: ['./orders.component.css'],
 })
 export class OrdersComponent {
+  Orders: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getOrders();
+  }
+
+  getOrders() {
+    const accessToken = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    });
+
+    this.http.get('http://localhost:8080/api/admin/getAllOrders', { headers }).subscribe(
+      (res: any) => {
+        console.log(res);
+        this.Orders = res.Orders;
+      },
+      (error) => {
+        console.error('Error getting orders', error);
+      }
+    );
+  }
+
+  // 
 
 }
